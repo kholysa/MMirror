6e88291aa3216aadae5dac808a168dac62f43ac6
kholysa <saif.elkholy@mail.mcgill.ca>
2016-06-29 23:38:55 +0300

    included stock info.cs

---
+++ MMirror/Controller/stockInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace MMirror.Controller
+{
+    class stockInfo
+    {
+        public stockInfo(){}
+        public void getStockFile()
+        {
+            //download the csv file and save it in the DATA folder
+            String URL = "http://download.finance.yahoo.com/d/quotes.csv?s=YHOO+GOOG+MSFT+AAPL&f=spva2a";
+            WebClient n = new WebClient();
+            //dleete the old file
+            File.Delete(@"..\..\Data\stockInfo.csv");
+            n.DownloadFile(URL, "stockInfo.csv");
+            //place the new stockInfo in the data folder
+            File.Move("stockInfo.csv", @"..\..\Data\stockInfo.csv");
+        }
+
+    }
+}
